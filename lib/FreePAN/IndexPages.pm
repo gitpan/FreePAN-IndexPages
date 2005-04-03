b4b6b663d8c476317642d269a89b9aeff2310e0a
劉康民 <gugod@gugod.org>
2005-04-03 14:53:35 +0000

    Import of GUGOD/FreePAN-IndexPages-0.02 from CPAN.
    gitpan-cpan-distribution: FreePAN-IndexPages
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         GUGOD/FreePAN-IndexPages-0.02.tar.gz
    gitpan-cpan-author:       GUGOD
    gitpan-cpan-maturity:     released

--- lib/FreePAN/IndexPages.pm
+++ lib/FreePAN/IndexPages.pm
@@ -4,7 +4,7 @@ use mixin 'FreePAN::Installer';
 use IO::All;
 use SVK;
 use SVK::XD;
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 const class_id => 'indexpages';
 const config_file => 'config/indexpages.yaml';
@@ -15,6 +15,14 @@ sub register {
     my $reg = shift;
     $reg->add(command => 'create_module_index',
               description => 'Create Module index page');
+    $reg->add(command => 'create_repository_index',
+              description => 'Create Repository index page');
+}
+
+sub handle_create_repository_index {
+    my @repos = map { $_->filename } io($self->hub->config->repos_base)->all_dirs;
+    my $output = $self->hub->template->process('repositories.html',repos => \@repos);
+    $output > io->catfile($self->hub->config->document_root,"repositories.html")->assert;
 }
 
 sub handle_create_module_index {
@@ -50,6 +58,18 @@ __templates/modules.html__
     <ul>
     [%- FOREACH key = modules.keys %]
     <li><a href="[% hub.config.svn_domain_name %]/[% modules.$key %]/[% key %]">[% key %]</a></li>
+    [% END -%]
+    </ul>
+    </body>
+</html>
+__templates/repositories.html__
+<html>
+    <title>FreePAN Repositories Index</title>
+    <body>
+    <h1>FreePAN Repositories Index</h1>
+    <ul>
+    [%- FOREACH repo = repos %]
+    <li><a href="[% hub.config.svn_domain_name %]/[% repo %]">[% repo %]</a></li>
     [% END -%]
     </ul>
     </body>
